f53f1607a536f3583a69b836e37125dee6f45955
Pedro Zequi <pedrosz@al.insper.edu.br>
2020-04-03 13:51:43 -0300

    ma

--- Apostas.py
+++ Apostas.py
@@ -1,40 +1,55 @@
-######################twelve############################################
-def twelve (dado1,dado2,soma_dados, fichas, aposta):
-    if soma_dados == 12:
-        x= aposta*30
-        x1 = fichas + x
-        print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(x, x1))
-    else:
-        x1 = fichas - aposta
-        print("Que pena! Você perdeu {0} fichas! E agora restam {1} fichas!".format(aposta, x1))
+import random
 
-######################anycraps#########################################
-def any_craps (dado1,dado2,soma_dados, fichas, aposta):
-    if soma_dados == 2 or soma_dados == 3 or soma_dados == 12:
-        y= aposta*7
-        y1= fichas + y
-        print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(y, y1))
-    else:
-        y1= fichas-aposta
-        print("Que pena! Você perdeu {0} fichas! E agora restam {1} fichas!".format(aposta,y1))
+tipo_de_aposta = str(input("Em qual fase você deseja apostar?"))
+if tipo_de_aposta == 'twelve':
+    aposta = int(input("Certo, você escolheu a aposta Twelve! Você possui {0} fichas, quanto você deseja apostar?".format(fichastotais)))
+    dado1 = random.randint (1,6)
+    dado2 = random.randint (1,6)
+    soma_dados = dado1+dado2
+    def twelve (dado1,dado2,soma_dados, fichastotais, aposta):
+        if soma_dados == 12:
+            x= aposta*30
+            fichastotais = fichastotais + x
+            print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(x, fichastotais))
+        else:
+            fichastotais = fichastotais - aposta
+            print("Que pena! Você perdeu {0} fichas! E agora restam {1} fichas!".format(aposta, fichastotais))
+        
+elif tipo_de_aposta == 'any craps':
+    aposta = int(input("Certo, você escolheu a aposta Any Craps! Você possui {0} fichas, quanto você deseja apostar?".format(fichastotais)))
+    dado1 = random.randint (1,6)
+    dado2 = random.randint (1,6)
+    soma_dados = dado1 + dado2
+    def any_craps (dado1,dado2,soma_dados, fichastotais, aposta):
+        if soma_dados == 2 or soma_dados == 3 or soma_dados == 12:
+            y= aposta*7
+            fichastotais = fichastotais + y
+            print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(y, fichastotais))
+        else:
+            fichastotais = fichastotais - aposta
+            print("Que pena! Você perdeu {0} fichas! E agora restam {1} fichas!".format(aposta, fichastotais))
 
-##########################field#####################################
-def field (dado1,dado2,soma_dados, fichas, aposta):
-    if soma_dados == 3 or soma_dados == 4 or soma_dados == 9 or soma_dados == 10 or soma_dados == 11:
-        p= aposta 
-        p1= fichas + aposta 
-        print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(p, p1))
-    elif soma_dados == 5 or soma_dados == 6 or soma_dados == 7 or soma_dados == 8:
-        p1= fichas-aposta
-        print("Que pena! Você perdeu {0} fichas! E agora restam {1}!".format(aposta,p1))
-    elif soma_dados == 2:
-        p= aposta*2
-        p1= fichas + p
-        print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(p,p1))
-    elif soma_dados == 12:
-        p= aposta*3
-        p1= fichas + p
-        print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(p, p1))
+elif tipo_de_aposta == 'field':
+    aposta = int(input(print("Certo, você escolheu a aposta Field! Você possui {0} fichas, quanto você deseja apostar?".format(fichastotais))))
+    dado1 = random.randint (1,6)
+    dado2 = random.randint (1,6)
+    soma_dados = dado1 + dado2 
+    def field (dado1,dado2,soma_dados, fichastotais, aposta):
+        if soma_dados == 3 or soma_dados == 4 or soma_dados == 9 or soma_dados == 10 or soma_dados == 11:
+            p= aposta 
+            fichastotais = fichastotais + aposta 
+            print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(p, fichastotais))
+        elif soma_dados == 5 or soma_dados == 6 or soma_dados == 7 or soma_dados == 8:
+            fichastotais= fichastotais - aposta
+            print("Que pena! Você perdeu {0} fichas! E agora restam {1}!".format(aposta,fichastotais))
+        elif soma_dados == 2:
+            p= aposta*2
+            fichastotais = fichastotais + p
+            print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(p,fichastotais))
+        elif soma_dados == 12:
+            p= aposta*3
+            fichastotais = fichastotais + p
+            print("Parabéns! Você ganhou {0} fichas! Agora você possui {1} fichas!".format(p, fichastotais))
 
 
  
